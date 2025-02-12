3d9addcfffaba4075559eb48f5dafa2e67054de1
sfoucher <samuel.foucher@gmail.com>
2025-02-12 11:03:31 -0500

    new content

--- process.sh
+++ process.sh
@@ -1,45 +1,45 @@
 quarto render --no-cache
 
 quarto convert 00-PriseEnMainPython.qmd
-jupyter nbconvert 00-PriseEnMainPython.ipynb  --to pdf --output-dir pdfs
-jupyter nbconvert 00-PriseEnMainPython.ipynb --to latex --output-dir latex
+#jupyter nbconvert 00-PriseEnMainPython.ipynb  --to pdf --output-dir pdfs
+#jupyter nbconvert 00-PriseEnMainPython.ipynb --to latex --output-dir latex
 mv 00-PriseEnMainPython.ipynb ./notebooks/
 
 quarto convert index.qmd
-jupyter nbconvert index.ipynb  --to pdf --output-dir pdfs
-jupyter nbconvert index.ipynb --to latex --output-dir latex
+#jupyter nbconvert index.ipynb  --to pdf --output-dir pdfs
+#jupyter nbconvert index.ipynb --to latex --output-dir latex
 
 quarto convert 00-auteurs.qmd
-jupyter nbconvert 00-auteurs.ipynb  --to pdf --output-dir pdfs
-jupyter nbconvert 00-auteurs.ipynb --to latex --output-dir latex
+#jupyter nbconvert 00-auteurs.ipynb  --to pdf --output-dir pdfs
+#jupyter nbconvert 00-auteurs.ipynb --to latex --output-dir latex
 
 quarto convert references.qmd
-jupyter nbconvert references.ipynb  --to pdf --output-dir pdfs
-jupyter nbconvert references.ipynb --to latex --output-dir latex
+#jupyter nbconvert references.ipynb  --to pdf --output-dir pdfs
+#jupyter nbconvert references.ipynb --to latex --output-dir latex
 
 quarto convert 01-ImportationManipulationImages.qmd
-jupyter nbconvert 01-ImportationManipulationImages.ipynb  --execute --to pdf --output-dir pdfs
-jupyter nbconvert 01-ImportationManipulationImages.ipynb --execute --to latex --output-dir latex
+#jupyter nbconvert 01-ImportationManipulationImages.ipynb  --execute --to pdf --output-dir pdfs
+#jupyter nbconvert 01-ImportationManipulationImages.ipynb --execute --to latex --output-dir latex
 mv 01-ImportationManipulationImages.ipynb ./notebooks/
 
 quarto convert 02-RehaussementVisualisationImages.qmd
-jupyter nbconvert 02-RehaussementVisualisationImages.ipynb  --execute --to pdf --output-dir pdfs
-jupyter nbconvert 02-RehaussementVisualisationImages.ipynb --execute --to latex --output-dir latex
+#jupyter nbconvert 02-RehaussementVisualisationImages.ipynb  --execute --to pdf --output-dir pdfs
+#jupyter nbconvert 02-RehaussementVisualisationImages.ipynb --execute --to latex --output-dir latex
 mv 02-RehaussementVisualisationImages.ipynb ./notebooks/
 
 quarto convert 03-TransformationSpectrales.qmd
-jupyter nbconvert 03-TransformationSpectrales.ipynb  --execute --to pdf --output-dir pdfs
-jupyter nbconvert 03-TransformationSpectrales.ipynb --execute --to latex --output-dir latex
+#jupyter nbconvert 03-TransformationSpectrales.ipynb  --execute --to pdf --output-dir pdfs
+#jupyter nbconvert 03-TransformationSpectrales.ipynb --execute --to latex --output-dir latex
 mv 03-TransformationSpectrales.ipynb ./notebooks/
 
 quarto convert 04-TransformationSpatiales.qmd
-jupyter nbconvert 04-TransformationSpatiales.ipynb  --execute --to pdf --output-dir pdfs
-jupyter nbconvert 04-TransformationSpatiales.ipynb --execute --to latex --output-dir latex
+#jupyter nbconvert 04-TransformationSpatiales.ipynb  --execute --to pdf --output-dir pdfs
+#jupyter nbconvert 04-TransformationSpatiales.ipynb --execute --to latex --output-dir latex
 mv 04-TransformationSpatiales.ipynb ./notebooks/
 
 quarto convert 05-ClassificationsSupervisees.qmd
-jupyter nbconvert 05-ClassificationsSupervisees.ipynb  --execute --to pdf --output-dir pdfs
-jupyter nbconvert 05-ClassificationsSupervisees.ipynb --execute --to latex --output-dir latex
+#jupyter nbconvert 05-ClassificationsSupervisees.ipynb  --execute --to pdf --output-dir pdfs
+#jupyter nbconvert 05-ClassificationsSupervisees.ipynb --execute --to latex --output-dir latex
 mv 05-ClassificationsSupervisees.ipynb ./notebooks/
 
 
